0ed053d0808881073aacad78577d6a4f440d4492
Alja Čekada <alja.cekada@gmail.com>
2023-03-06 20:26:07 +0100

    create home page

--- src/page-load.js
+++ src/page-load.js
@@ -1,10 +1,185 @@
+function createHeader() {
+  const header = document.createElement('header');
+  header.classList.add('hero-img');
+
+  header.appendChild(createNav());
+
+  const heroText = document.createElement('div');
+  heroText.classList.add('hero-text');
+
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Restaurant page';
+  heroText.appendChild(h1);
+
+  const p = document.createElement('p');
+  p.textContent = 'Best food in town!';
+  heroText.appendChild(p);
+
+  header.appendChild(heroText);
+
+  return header;
+}
+
+function setActiveButton() {
+  const navItems = document.querySelectorAll('.nav-item');
+
+  navItems.forEach((item) => {
+    if (item !== item.classList.contains('active')) {
+      item.classList.add('active');
+    } else {
+      item.classList.remove('active');
+    }
+  });
+}
+
+function createNav() {
+  const nav = document.createElement('nav');
+
+  const homeItem = document.createElement('div');
+  homeItem.classList.add('nav-item');
+  homeItem.textContent = 'Home';
+  homeItem.addEventListener('click', (e) => {
+    if (e.target.classList.contains('active')) return;
+    setActiveButton(homeItem);
+  });
+
+  const menuItem = document.createElement('div');
+  menuItem.classList.add('nav-item');
+  menuItem.textContent = 'Menu';
+  menuItem.addEventListener('click', (e) => {
+    if (e.target.classList.contains('active')) return;
+    setActiveButton(menuItem);
+  });
+
+  const contactItem = document.createElement('div');
+  contactItem.classList.add('nav-item');
+  contactItem.textContent = 'Contact';
+  contactItem.addEventListener('click', (e) => {
+    if (e.target.classList.contains('active')) return;
+    setActiveButton(contactItem);
+  });
+
+  nav.appendChild(homeItem);
+  nav.appendChild(menuItem);
+  nav.appendChild(contactItem);
+
+  return nav;
+}
+
+function createRatingSection() {
+  const ratingSection = document.createElement('div');
+  ratingSection.classList.add('rating-section');
+
+  const ratingBoxes = [
+    {
+      stars: 3,
+      name: 'John Doe',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod lectus vitae nunc luctus dignissim.',
+    },
+    {
+      stars: 4,
+      name: 'Jane Doe',
+      text: 'Pellentesque blandit, augue vel accumsan ultrices, augue odio bibendum mi, nec lobortis erat lorem eget urna.',
+    },
+    {
+      stars: 5,
+      name: 'Bob Smith',
+      text: 'Suspendisse sed maximus massa. In convallis quam ac nulla bibendum, in varius ante fringilla.',
+    },
+  ];
+
+  ratingBoxes.forEach((box) => {
+    const ratingBox = document.createElement('div');
+    ratingBox.classList.add('rating-box');
+
+    const ratingStars = document.createElement('div');
+    ratingStars.classList.add('rating-stars');
+
+    for (let i = 0; i < box.stars; i++) {
+      const star = document.createElement('span');
+      star.classList.add('star');
+      star.innerHTML = '&#9733;';
+      ratingStars.appendChild(star);
+    }
+
+    for (let i = box.stars; i < 5; i++) {
+      const star = document.createElement('span');
+      star.classList.add('star');
+      star.innerHTML = '&#9734;';
+      ratingStars.appendChild(star);
+    }
+
+    const ratingName = document.createElement('div');
+    ratingName.classList.add('rating-name');
+    ratingName.innerHTML = box.name;
+
+    const ratingText = document.createElement('div');
+    ratingText.classList.add('rating-text');
+    ratingText.innerHTML = box.text;
+
+    ratingBox.appendChild(ratingStars);
+    ratingBox.appendChild(ratingName);
+    ratingBox.appendChild(ratingText);
+    ratingSection.appendChild(ratingBox);
+  });
+
+  return ratingSection;
+}
+
+function createDelieverySection() {
+  const delieveryContainer = document.createElement('div');
+  delieveryContainer.classList.add('delievery-container');
+
+  const h1 = document.createElement('h1');
+  h1.textContent = 'order delievery';
+  delieveryContainer.appendChild(h1);
+
+  const delieveryLogo1 = document.createElement('img');
+  delieveryLogo1.classList.add('delievery-logo');
+  delieveryLogo1.setAttribute('src', 'img/wolt.png');
+  delieveryContainer.appendChild(delieveryLogo1);
+
+  const delieveryLogo2 = document.createElement('img');
+  delieveryLogo2.classList.add('delievery-logo');
+  delieveryLogo2.setAttribute('src', 'img/uber-eats.png');
+  delieveryContainer.appendChild(delieveryLogo2);
+
+  return delieveryContainer;
+}
+
+function createFooter() {
+  const footerContainer = document.createElement('div');
+  footerContainer.classList.add('footer-container');
+
+  const wavesSvg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#273036" fill-opacity="1" d="M0,256L48,234.7C96,213,192,171,288,133.3C384,96,480,64,576,69.3C672,75,768,117,864,128C960,139,1056,117,1152,106.7C1248,96,1344,96,1392,96L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>';
+  footerContainer.innerHTML = wavesSvg;
+
+  const footer = document.createElement('footer');
+
+  const a = document.createElement('a');
+  a.setAttribute('href', 'https://github.com/Alchuu00');
+
+  const githubLogo = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg>';
+  const githubDiv = document.createElement('div');
+  githubDiv.innerHTML = githubLogo;
+  a.appendChild(githubDiv);
+  footer.appendChild(a);
+
+  const p = document.createElement('p');
+  p.textContent = 'Developed by Alchuu00';
+  footer.appendChild(p);
+
+  footerContainer.appendChild(footer);
+  return footerContainer;
+}
+
 function pageLoad() {
   const content = document.getElementById('content');
-  console.log(content);
 
-  const child = document.createElement('div');
-
-  content.appendChild(child);
+  content.appendChild(createHeader());
+  content.appendChild(createRatingSection());
+  content.appendChild(createDelieverySection());
+  content.appendChild(createFooter());
 }
 
 export default pageLoad;
